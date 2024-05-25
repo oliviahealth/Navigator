c1405b8f830f94ccafdbc5611c8b8634d2e9bdd2
vedarth31 <vedarth31@gmail.com>
2024-05-25 00:46:40 -0500

    added get/del routes for user; documented api routes

--- src/app/api/tab-one/communication-log/route.ts
+++ src/app/api/tab-one/communication-log/route.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../lib/prisma';
 
+/**
+ * Creates a CommunicationLog instance
+ * @param req userId (URL param), dateTime (DateTime), method (String), organizationPerson(String), purpose(String), notes(String), followUpNeeded(String)
+ * @returns the new CommunicationLog instance or an error
+ */
 export async function POST(req: NextRequest) {
 
     const { searchParams } = new URL(req.url);
     const user_id = searchParams.get('user_id');
 
+    // Check if the api request has a user_id included as a parameter in the URL
     if (!user_id) {
         return NextResponse.json({ message: 'user_id is required' }, { status: 400 });
     }
@@ -22,6 +28,8 @@ export async function POST(req: NextRequest) {
             notes,
             followUpNeeded,
         }
+
+        // Creates new CommunicationLog instance
         const newCommunicationLog = await prisma.communicationLog.create({
             data: newCommunicationLogData,
         })
@@ -29,19 +37,25 @@ export async function POST(req: NextRequest) {
         return NextResponse.json(newCommunicationLog, { status: 201 })
     } catch (error) {
         if (error instanceof Error) {
-            return NextResponse.json({ error: 'Error creating user', message: error.message }, { status: 500 });
+            return NextResponse.json({ error: 'Error creating CommunicationLog', message: error.message }, { status: 500 });
         } else {
-            return NextResponse.json({ error: 'Error creating user', message: 'Unknown error' }, { status: 500 });
+            return NextResponse.json({ error: 'Error creating CommunicationLog', message: 'Unknown error' }, { status: 500 });
         }
     }
 }
 
+/**
+ * Gets CommunicationLog instance(s) for a given user
+ * @param req id & user_id (URL params)
+ * @returns CommunicationLog for a given id if id specified, or all CommunicationLogs for the user, or an error
+ */
 export async function GET(req: NextRequest) {
 
     const { searchParams } = new URL(req.url);
     const user_id = searchParams.get('user_id');
     const id = searchParams.get('id');
 
+    // Check if the api request has a user id included as a parameter in the URL
     if (!user_id) {
         return NextResponse.json({ message: 'user_id is required' }, { status: 400 });
     }
@@ -55,42 +69,56 @@ export async function GET(req: NextRequest) {
             return NextResponse.json({ message: 'User not found.' }, { status: 404 });
         }
 
+        // If there is a specified id, return the CommunicationLog for the id
         if (id) {
             const communicationLog = await prisma.communicationLog.findFirst({
                 where: { userId: user_id, id: id },
             });
 
+            // Check if there is an existing CommunicationLog for the specified id
             if (!communicationLog) {
                 return NextResponse.json({ message: 'Invalid communicationLog id' }, { status: 400 });
             }
 
             return NextResponse.json(communicationLog, { status: 200 });
 
+            // If id not in URL param, then return all CommunicationLogs for the user_id
         } else {
             const communicationLogList = await prisma.communicationLog.findMany({
                 where: { userId: user_id },
             });
 
+            if (!communicationLogList) {
+                return NextResponse.json({ message: 'No CommunicationLogs for this user' }, { status: 400 });
+            }
+
             return NextResponse.json(communicationLogList, { status: 200 });
         }
     } catch (error) {
         if (error instanceof Error) {
-            return NextResponse.json({ error: 'Error creating user', message: error.message }, { status: 500 });
+            return NextResponse.json({ error: 'Error creating CommunicationLog', message: error.message }, { status: 500 });
         } else {
-            return NextResponse.json({ error: 'Error creating user', message: 'Unknown error' }, { status: 500 });
+            return NextResponse.json({ error: 'Error creating CommunicationLog', message: 'Unknown error' }, { status: 500 });
         }
     }
 }
 
+/**
+ * Updates a CommunicationLog instance
+ * @param req id & userId (URL params), dateTime (DateTime), method (String), organizationPerson(String), purpose(String), notes(String), followUpNeeded(String)
+ * @returns the updated CommunicationLog instance or an error
+ */
 export async function PUT(req: NextRequest) {
     const { searchParams } = new URL(req.url);
     const user_id = searchParams.get('user_id');
     const id = searchParams.get('id');
 
+    // Check if the api request has a user_id included as a parameter in the URL
     if (!user_id) {
         return NextResponse.json({ message: 'user_id is required' }, { status: 400 });
     }
 
+    // Check if the api request has an id included as a parameter in the URL
     if (!id) {
         return NextResponse.json({ message: 'id is required' }, { status: 400 });
     }
@@ -98,6 +126,7 @@ export async function PUT(req: NextRequest) {
     try {
         const { dateTime, method, organizationPerson, purpose, notes, followUpNeeded } = await req.json();
 
+        // Check if a CommunicationLog with this id exists in the db
         const existingCommunicationLog = await prisma.communicationLog.findUnique({
             where: {
                 id: id,
@@ -109,20 +138,23 @@ export async function PUT(req: NextRequest) {
             return NextResponse.json({ error: 'Communication log not found' }, { status: 404 });
         }
 
+        const updatedCommunicationLogData = {
+            dateTime,
+            method,
+            organizationPerson,
+            purpose,
+            notes,
+            followUpNeeded,
+            dateModified: new Date(),
+        }
+
+        // Update the CommunicationLog
         const updatedCommunicationLog = await prisma.communicationLog.update({
             where: {
                 id: id,
                 userId: user_id,
             },
-            data: {
-                dateTime,
-                method,
-                organizationPerson,
-                purpose,
-                notes,
-                followUpNeeded,
-                dateModified: new Date(),
-            },
+            data: updatedCommunicationLogData
         });
 
         return NextResponse.json(updatedCommunicationLog, { status: 200 });
@@ -135,21 +167,29 @@ export async function PUT(req: NextRequest) {
     }
 }
 
+/**
+ * Deletes a CommunicationLog instance from the db
+ * @param req id  & user_id (URL params)
+ * @returns message indiciating user deletion or an error
+ */
 export async function DELETE(req: NextRequest) {
 
     const { searchParams } = new URL(req.url);
     const user_id = searchParams.get('user_id')
     const id = searchParams.get('id');
 
+    // Check if the api request has a user_id included as a parameter in the URL
     if (!user_id) {
         return NextResponse.json({ message: 'user_id is required' }, { status: 400 });
     }
 
+    // Check if the api request has a id included as a parameter in the URL
     if (!id) {
         return NextResponse.json({ message: 'id is required' }, { status: 400 });
     }
 
     try {
+        // Check if a CommunicationLog instance with this id exists in the db
         const existingCommunicationLog = await prisma.communicationLog.findUnique({
             where: {
                 id: id,
@@ -161,6 +201,7 @@ export async function DELETE(req: NextRequest) {
             return NextResponse.json({ error: 'Communication log not found' }, { status: 404 });
         }
 
+        // Delete the CommunicationLog
         await prisma.communicationLog.delete({
             where: {
                 id: id,
