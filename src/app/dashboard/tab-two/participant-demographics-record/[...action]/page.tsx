bd0c803d94bee504d824d1f96c9e482872a1a83c
vedarth31 <vedarth31@gmail.com>
2024-05-27 17:50:19 -0500

    created Prisma Schema for ParticipantDemographicsRecord

--- src/app/dashboard/tab-two/participant-demographics-record/[...action]/page.tsx
+++ src/app/dashboard/tab-two/participant-demographics-record/[...action]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRouter, useParams } from 'next/navigation'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useFieldArray, useForm } from "react-hook-form";
