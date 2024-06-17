f00aa1f618911c1333fd7624fdbc8c0ab01800bb
Sebastian Oberg <Sebastianoberg2002@gmail.com>
2024-06-17 15:27:18 -0500

    Added partial definitions and frontend

--- src/app/dashboard/tab-nine/housing-security-home-visit-form/[...action]/page.tsx
+++ src/app/dashboard/tab-nine/housing-security-home-visit-form/[...action]/page.tsx
@@ -8,7 +8,9 @@ import { useForm } from "react-hook-form";
 import {
     HousingSecurityHomeVisitResponseSchema,
     IHousingSecurityHomeVisitInputs,
-    HousingSecurityHomeVisitInputsSchema
+    HousingSecurityHomeVisitInputsSchema,
+    YesNoEnum,
+    YesNoDidNotAskEnum
 } from "../definitions";
 
 import useAppStore from "@/lib/useAppStore";
@@ -161,6 +163,102 @@ const HousingSecurityHomeVisit: React.FC = () => {
                         </span>
                     )}
                 </div>
+
+                <div className="space-y-3">
+                    <p className="font-semibold">Do you have health insurance coverage? </p>
+                    <div className="flex items-center gap-x-12">
+                        {YesNoEnum.options.map(option => (
+                            <label key={option} className="flex items-center">
+                                <input
+                                    {...register("haveHealhInsurance")}
+                                    className="form-radio"
+                                    type="radio"
+                                    value={option}
+                                />
+                                <span className="ml-2">{option}</span>
+                            </label>
+                        ))}
+                    </div>
+                    {errors.haveHealhInsurance && (
+                        <span className="label-text-alt text-red-500">
+                            {errors.haveHealhInsurance.message}
+                        </span>
+                    )}
+                </div>
+                
+
+
+                <div className="space-y-3">
+                    <p className="font-semibold">Do you have any concerns about your child's development, behavior, or learning? </p>
+                    <div className="flex items-center gap-x-12">
+                        {YesNoDidNotAskEnum.options.map(option => (
+                            <label key={option} className="flex items-center">
+                                <input
+                                    {...register("childConcerns")}
+                                    className="form-radio"
+                                    type="radio"
+                                    value={option}
+                                />
+                                <span className="ml-2">{option}</span>
+                            </label>
+                        ))}
+                    </div>
+                    {errors.childConcerns && (
+                        <span className="label-text-alt text-red-500">
+                            {errors.childConcerns.message}
+                        </span>
+                    )}
+                </div>
+
+
+
+
+                <div className="space-y-3">
+                    <p className="font-semibold">Since our last visit, have you taken your child to the hospital emergency room? </p>
+                    <div className="flex items-center gap-x-12">
+                        {YesNoEnum.options.map(option => (
+                            <label key={option} className="flex items-center">
+                                <input
+                                    {...register("takenChildToEr")}
+                                    className="form-radio"
+                                    type="radio"
+                                    value={option}
+                                />
+                                <span className="ml-2">{option}</span>
+                            </label>
+                        ))}
+                    </div>
+                    {errors.takenChildToEr && (
+                        <span className="label-text-alt text-red-500">
+                            {errors.takenChildToEr.message}
+                        </span>
+                    )}
+                </div>
+                
+
+
+                <div className="space-y-3">
+                    <p className="font-semibold">Since our last visit, has your child had any well-child visits? </p>
+                    <div className="flex items-center gap-x-12">
+                        {YesNoEnum.options.map(option => (
+                            <label key={option} className="flex items-center">
+                                <input
+                                    {...register("wellChildVisits")}
+                                    className="form-radio"
+                                    type="radio"
+                                    value={option}
+                                />
+                                <span className="ml-2">{option}</span>
+                            </label>
+                        ))}
+                    </div>
+                    {errors.wellChildVisits && (
+                        <span className="label-text-alt text-red-500">
+                            {errors.wellChildVisits.message}
+                        </span>
+                    )}
+                </div>
+
 
                 <div className="flex justify-center py-4">
                     <button
--- src/app/dashboard/tab-nine/housing-security-home-visit-form/definitions.ts
+++ src/app/dashboard/tab-nine/housing-security-home-visit-form/definitions.ts
@@ -1,10 +1,26 @@
 import { z } from "zod";
 
+export const YesNoEnum = z.enum(["Yes", "No"]);
+export const YesNoDidNotAskEnum = z.enum(["Yes", "No", "Did not ask"]);
+export const WellChildVisitsEnum = z.enum(["Newborn", "3 - 7 days old", "2 - 4 weeks old", "2 - 3 months old", "4 - 5 months old", "6 - 7 months old", 
+    "9 - 10 months old", "12 - 13 months old", "15 - 16 months old", "18 - 19 months old", "2 - 2.5 years old", "3 - 3.5 years old", "4 - 4.5 years old"]);
+export const InjuryOtherReasonEnum = z.enum(["Injury", "Other Reason"]);
+
 export const HousingSecurityHomeVisitInputsSchema = z.object({
     participantName: z.string().min(1, "Participant name is required"),
     caseId: z.string().min(1, "CaseID is required"),
     dateOfVisit: z.union([z.date(), z.string().min(1, "Date of Visit is required")]),
     staffName: z.string().min(1, "Staff name is required"),
+    haveHealhInsurance: YesNoEnum,
+    childConcerns: YesNoDidNotAskEnum, 
+    takenChildToEr: YesNoEnum, 
+
+    erVisitDate: z.union([z.date(), z.string().min(1, "ER Visit Date required")]),
+    erVisitReason: InjuryOtherReasonEnum,
+
+    childName: z.string().min(1, "Child name is required"),
+    wellChildVisits: YesNoEnum  
+    // Need to add the logic so that if question is not applicable it can be left as blank
 });
 export type IHousingSecurityHomeVisitInputs = z.infer<typeof HousingSecurityHomeVisitInputsSchema>
 
