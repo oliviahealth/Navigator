2c39f6c410c9edafd1fc08372cb28c4bd786b1e6
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-07-29 20:02:00 -0500

    add submission component

--- src/app/dashboard/logs-and-forms/page.tsx
+++ src/app/dashboard/logs-and-forms/page.tsx
@@ -1,9 +1,13 @@
 import Accordion from "@/components/dashboard/Accordion";
+import Submissions from "@/components/dashboard/Submissions";
 
 const Tab: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <Accordion title="Form name">Tab</Accordion>
+      <Accordion title="Form name">
+        Tab content
+        <Submissions></Submissions>
+      </Accordion>
     </div>
   );
 };
