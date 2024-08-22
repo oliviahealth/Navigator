a0f2f4cb729bfee5b6b35ce6287a29bdb6d43f8e
vedarth31 <vedarth31@gmail.com>
2024-08-22 09:14:10 -0500

    merged w main; fixed styling

--- src/app/dashboard/medications/page.tsx
+++ src/app/dashboard/medications/page.tsx
@@ -16,21 +16,33 @@ const Tab: React.FC = () => {
     <div className="flex flex-col">
       <Accordion title="Current Medication List">
         {selectedCurrentMedicationListSubmission && (
-          <div>
-            {Array.isArray(selectedCurrentMedicationListSubmission.currentMedicationList) && selectedCurrentMedicationListSubmission.currentMedicationList.length > 0 ? (
-              <div className="flex flex-wrap gap-4 mt-2">
-                {selectedCurrentMedicationListSubmission.currentMedicationList.map((entry: any, index: number) => (
-                  <div key={index} className="border p-2 rounded-md flex-1 min-w-[200px]">
-                    <p><strong>Medication:</strong> {entry.name}</p>
-                    <p><strong>Dose:</strong> {entry.dose}</p>
-                    <p><strong>Location:</strong> {entry.prescriber}</p>
-                    <p><strong>Notes:</strong> {entry.notes || 'N/A'}</p>
-                  </div>
-                ))}
+          <div className="flex flex-row gap-4">
+            <div className="flex-1">
+              {Array.isArray(selectedCurrentMedicationListSubmission.currentMedicationList) && selectedCurrentMedicationListSubmission.currentMedicationList.length > 0 ? (
+                <div className="grid grid-cols-1 gap-4">
+                  {selectedCurrentMedicationListSubmission.currentMedicationList.map((entry: any, index: number) => (
+                    <div key={index} className="bento-inner">
+                      <p><strong>Medication:</strong> {entry.name}</p>
+                      <p><strong>Dose:</strong> {entry.dose}</p>
+                      <p><strong>Location:</strong> {entry.prescriber}</p>
+                      <p><strong>Notes:</strong> {entry.notes || 'N/A'}</p>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p>No current medication lists available.</p>
+              )}
+            </div>
+            <div className="flex flex-col flex-1 gap-4">
+              <div className="flex-1 bento-inner">
+                <h2 className="font-bold">Additional Notes</h2>
+                <p>{selectedCurrentMedicationListSubmission.notes}</p>
               </div>
-            ) : (
-              <p>No current medication lists available.</p>
-            )}
+              <div className="flex-1 bento-inner">
+                <h2 className="font-bold">Staff Notes</h2>
+                <p>{selectedCurrentMedicationListSubmission.staffNotes}</p>
+              </div>
+            </div>
           </div>
         )}
         <Submission
